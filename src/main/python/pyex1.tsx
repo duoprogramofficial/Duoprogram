679cd2f726a5965a14e7ae853e63828606710815
lucasfelezdevlop <lucasfelezdevlop@gmail.com>
2023-11-26 01:52:21 +0100

    added in pythonindex.tsx that you need an ide and python and updated pyex1.tsx

--- src/main/python/pyex1.tsx
+++ src/main/python/pyex1.tsx
@@ -10,12 +10,9 @@ function App() {
   
   return (
     <div className="container">
-      <h1>Welcome to duoprogram</h1>
-      <h3>you choosed to program in python, 
-      that's a good choice to learn as a starter, it can do many things such as math and physics!
-      even making games!</h3>
+      <h1>Basic hello world</h1>
        
-      <Link to="/py/ex1"><button>L'ets learn!</button></Link>
+      The famous basic 
       </div>
   );
   
--- src/main/python/pythonindex.tsx
+++ src/main/python/pythonindex.tsx
@@ -14,6 +14,7 @@ function App() {
       <h3>you choosed to program in python, 
       that's a good choice to learn as a starter, it can do many things such as math and physics!
       even making games!</h3>
+      <h2>if you want to learn that you need to download an IDE like VScode and install python</h2>
        
       <Link to="/py/ex1"><button>L'ets learn!</button></Link>
       </div>
