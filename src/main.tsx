9aa484b92586d0f36464e438173cdfca57994f52
lucasfelezdevlop <lucasfelezdevlop@gmail.com>
2023-11-26 01:21:48 +0100

    Some new things

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Learn from './main/learn';
 import Pythonindex from './main/python/pythonindex'
+import Pyex1 from './main/python/pyex1'
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
@@ -15,6 +16,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/learn" element={<Learn />} />
         <Route path="/py/pyindex" element={<Pythonindex />} />
+        <Route path="/py/pyex1" element={<Pyex1 />} />
       </Routes>
     </Router>
   </React.StrictMode>
